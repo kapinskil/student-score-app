867559d3df85d959fbba9ea62584909718a609b0
Kapinskil <kapinski.l@gmail.com>
2025-05-26 15:21:24 +0200

    copilot improvments

--- Program.cs
+++ Program.cs
@@ -1,81 +1,57 @@
 ﻿using System;
+using System.Collections.Generic;
 
 // initialize variables - graded assignments 
 int examAssignments = 5;
 
-int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
-int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
-int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
-int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
-int[] beckyScores = new int[] { 92, 91, 90, 91, 92, 92, 92 };
-int[] chrisScores = new int[] { 84, 86, 88, 90, 92, 94, 96, 98 };
-int[] ericScores = new int[] { 80, 90, 100, 80, 90, 100, 80, 90 };
-int[] gregorScores = new int[] { 91, 91, 91, 91, 91, 91, 91 };
+// Use a dictionary for easier student lookup
+var studentScoresDict = new Dictionary<string, int[]>
+{
+    { "Sophia", new int[] { 90, 86, 87, 98, 100, 94, 90 } },
+    { "Andrew", new int[] { 92, 89, 81, 96, 90, 89 } },
+    { "Emma", new int[] { 90, 85, 87, 98, 68, 89, 89, 89 } },
+    { "Logan", new int[] { 90, 95, 87, 88, 96, 96 } },
+    { "Becky", new int[] { 92, 91, 90, 91, 92, 92, 92 } },
+    { "Chris", new int[] { 84, 86, 88, 90, 92, 94, 96, 98 } },
+    { "Eric", new int[] { 80, 90, 100, 80, 90, 100, 80, 90 } },
+    { "Gregor", new int[] { 91, 91, 91, 91, 91, 91, 91 } }
+};
 
-//students names
 string[] studentsNames = new string[] { "Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor" };
-
-int[] studentsScores = new int[10];
-
-string currentStudentLetterGrade = "";
 
 Console.WriteLine("Student\t\tExam Score\tOverall\t\tGrade\t\tExtra Credits\n");
 
-foreach (string name in studentsNames)
+foreach (string studentName in studentsNames)
 {
-    string currenyStudentName = name;
-
-    if (currenyStudentName == "Sophia")
-        studentsScores = sophiaScores;
-    else if (currenyStudentName == "Andrew")
-        studentsScores = andrewScores;
-    else if (currenyStudentName == "Emma")
-        studentsScores = emmaScores;
-    else if (currenyStudentName == "Logan")
-        studentsScores = loganScores;
-    else if (currenyStudentName == "Becky")
-        studentsScores = beckyScores;
-    else if (currenyStudentName == "Chris")
-        studentsScores = chrisScores;
-    else if (currenyStudentName == "Eric")
-        studentsScores = ericScores;
-    else if (currenyStudentName == "Gregor")
-        studentsScores = gregorScores;
-    else
+    if (!studentScoresDict.TryGetValue(studentName, out int[]? scores))
         continue;
 
-
     int sumAssignmentScores = 0;
-    decimal currentStudentGrade = 0;
-
-    int gradedAssignments = 0;
-
-    decimal examScore = 0;
     decimal sumExamScores = 0;
-
-    decimal extraCredits = 0;
     decimal sumExtraCredits = 0;
 
-    foreach (int score in studentsScores)
+    for (int i = 0; i < scores.Length; i++)
     {
-        gradedAssignments++;
-        if (gradedAssignments <= examAssignments)
+        if (i < examAssignments)
         {
-            sumAssignmentScores += score;
-            sumExamScores += score;
+            sumAssignmentScores += scores[i];
+            sumExamScores += scores[i];
         }
         else
-        { 
-            sumExtraCredits += score / 10;
-            sumAssignmentScores += score / 10;
+        {
+            decimal extraCredit = (decimal)scores[i] / 10;
+            sumExtraCredits += extraCredit;
+            sumAssignmentScores += (int)extraCredit;
         }
-            
     }
 
-    currentStudentGrade = (decimal)sumAssignmentScores / examAssignments;
-    examScore = (decimal)sumExamScores / examAssignments;
-    extraCredits = (decimal)sumExtraCredits / examAssignments;
+    decimal currentStudentGrade = (decimal)sumAssignmentScores / examAssignments;
+    decimal examScore = sumExamScores / examAssignments;
+    decimal extraCredits = sumExtraCredits / examAssignments;
 
+    decimal gradeWithoutExtraCredits = currentStudentGrade - extraCredits; // new variable
+
+    string currentStudentLetterGrade;
     if (currentStudentGrade >= 97)
         currentStudentLetterGrade = "A+";
     else if (currentStudentGrade >= 93)
@@ -103,10 +79,8 @@ foreach (string name in studentsNames)
     else
         currentStudentLetterGrade = "F";
 
-    // ("Student\t\tExam Score\t\tOverall\t\tGrade|t|tExtra Credits\n");
-    Console.WriteLine($"{currenyStudentName}\t\t{examScore}\t\t{currentStudentGrade}\t\t{currentStudentLetterGrade}\t\t {currentStudentGrade-extraCredits} ({extraCredits} pts)");
+    Console.WriteLine($"{studentName}\t\t{examScore:F1}\t\t{currentStudentGrade:F1}\t\t{currentStudentLetterGrade}\t\t{gradeWithoutExtraCredits:F1} ({extraCredits:F1} pts)");
 }
-
 
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine();
